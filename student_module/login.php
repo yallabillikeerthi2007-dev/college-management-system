51f899284732e086fc5d62c705d6ee0daa23cdee
Jyotshna <korukondajyotshna247@gmail.com>
2026-05-26 06:20:26 +0530

    Added student_module

---
+++ student_module/login.php
@@ -0,0 +1,34 @@
+<?php
+include 'db.php';
+$role   = $_POST['role'];
+$uname  = $_POST['uname'];
+$pass   = $_POST['password'];
+
+// ADMIN LOGIN
+if ($role == "Admin") {
+    $sql = "SELECT * FROM admin WHERE username='$uname' AND password='$pass'";
+    $result = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        header("Location: admin_dashboard.php");
+        exit;
+    } else {
+        echo "Invalid Admin Details";
+    }
+}
+
+// STUDENT LOGIN
+else if ($role == "Student") {
+    $sql = "SELECT * FROM student WHERE Name='$uname' AND Id='$pass'";
+    $result = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        header("Location: student_dashboard.php?id=$pass");
+        exit;
+    } else {
+        echo "Invalid Student Details";
+    }
+}
+
+mysqli_close($con);
+?>

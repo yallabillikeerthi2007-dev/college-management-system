c315f231f9cd892b9d86021a6ec5cbb15183adc1
Jyoshu2009 <korukondajyotshna247@gmail.com>
2026-05-28 18:17:43 +0530

    Delete student_module/db.php

--- student_module/db.php
+++
@@ -1,8 +0,0 @@
-<?php
- //connecting to the database
-    $con=mysqli_connect("127.0.0.1:3307","root","","college_management");
-    if(!$con)
-        {
-            die("connection failed". mysqli_connect_error());
-        }
-       ?>
